5d28df33448607a3b6b3c0bf13ec70deb8a4fa1c
Nuno Farinha <nadfarinha@gmail.com>
2019-01-22 00:33:12 +0000

    Initial commit for extension

--- library/library.rb
+++ library/library.rb
@@ -1,15 +1,69 @@
 class Library
 
 
-  attr_reader :books
+  attr_accessor :book_title, :rentee_name, :rental_date, :book_collection
 
-  def initialize(books)
-    @books = books
+  def initialize(book_title, rentee_name, rental_date)
+
+    @book_title = book_title
+    @rentee_name = rentee_name
+    @rental_date = rental_date
+    @book = {
+        title: book_title,
+        rental_details: {
+          student_name: rentee_name,
+          date: rental_date
+        }
+      }
+    @book_collection = [@book]
   end
 
-def get_books()
-  return @books
-end
+#* Create a getter for the books
+  def get_books()
+    return @book_collection
+  end
+
+#* Create a method that takes in a book title and returns all of the information about that book.
+  def search_book(title)
+    for books in @book_collection
+      return books if books[:title] == title
+    end
+  end
+
+#* Create a method that takes in a book title and returns only the rental details for that book.
+  def search_rental_details(title)
+    for books in @book_collection
+      return books[:rental_details] if books[:title] == title
+    end
+  end
+
+#* Create a method that takes in a book title and adds it to our book list (add a new hash for the book with the student name and date being left as empty strings)
+  def add_new_book(new_title)
+    @book = {
+      title: new_title,
+      rental_details: {
+      student_name: "",
+      date: ""
+      }
+    }
+    @book_collection.push(@book)
+  end
+
+#* Create a method that changes the rental details of a book by taking in the title of the book, the student renting it and the date it's due to be returned.
+  def change_rental(book_title, student_name, return_date)
+
+=begin
+#This code will not work if it needs to cater for book title not found
+    new_rental = search_rental_details(book_title)
+    new_rental[:student_name] = student_name
+    new_rental[:date] = return_date
+=end
+    return if book_title == new_rental = search_rental_details(book_title)
+    new_rental[:student_name] = student_name
+    new_rental[:date] = return_date
+  end
 
 
+
+  # END CLASS
 end
--- library/specs/library_spec.rb
+++ library/specs/library_spec.rb
@@ -5,35 +5,46 @@ require_relative('../library')
 class TestLibrary < MiniTest::Test
 
   def setup
-    @city_library = Library.new(
-      [
-        {
-          title: "lord_of_the_rings",
-          rental_details: {
-            student_name: "Jeff",
-            date: "01/12/16"
-          }
-        },
-        {
-          title: "Brave New World",
-          rental_details: {
-            student_name: "John",
-            date: "04/15/16"
-          }
-        },
-        {
-          title: "Dune",
-          rental_details: {
-            student_name: "Kate",
-            date: "07/20/16"
-          }
+
+    @edin_library = Library.new("lord_of_the_rings", "Jeff", "01/12/16")
+
+    @model_library =
+    [
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+          student_name: "Jeff",
+          date: "01/12/16"
         }
-      ]
-    )
+      }
+    ]
   end
 
   def test_get_books
-    assert_equal( @city_library, @city_library.get_books() )
+    assert_equal(@model_library, @edin_library.get_books())
+  end
+
+  def test_search_book
+    model_book = @model_library[0]
+    assert_equal(model_book, @edin_library.search_book("lord_of_the_rings"))
+  end
+
+  def test_search_rental_details
+    book_rental = @model_library[0][:rental_details]
+    assert_equal(book_rental, @edin_library.search_rental_details("lord_of_the_rings"))
+  end
+
+  def test_add_new_book
+    @edin_library.add_new_book("Dune")
+    assert_equal(2, @edin_library.book_collection.length())
+  end
+
+  def test_change_rental
+    @edin_library.change_rental("lord_of_the_rings", "Dan", "15/12/16")
+    book_details = @edin_library.search_rental_details("lord_of_the_rings")
+
+    assert_equal("Dan", book_details[:student_name])
+    assert_equal("15/12/16", book_details[:date])
   end
 
 end
